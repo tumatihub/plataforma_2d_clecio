77ebeddf6fd3e0525b08c446e96325520bd53b48
tumatihub <jgsoaresdias@gmail.com>
2024-05-22 22:22:23 -0300

    Ep12 - Gerenciar os States Animations na Godot 4.0

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,8 +7,10 @@ const JUMP_FORCE = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_jumping := false
+var is_hurted := false
 var player_life := 10
 var knockback_vector := Vector2.ZERO
+var direction
 
 @onready var animation := $anim as AnimatedSprite2D
 @onready var remote_transform := $remote as RemoteTransform2D
@@ -27,21 +29,17 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 		animation.scale.x = direction
-		if !is_jumping:
-			animation.play("run")
-	elif is_jumping:
-		animation.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animation.play("idle")
 	
 	if knockback_vector != Vector2.ZERO:
 		velocity = knockback_vector
 	
+	_set_state()
 	move_and_slide()
 
 func _on_hurtbox_body_entered(body: Node2D) -> void:
@@ -69,6 +67,24 @@ func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
 		knockback_tween.parallel().tween_property(self, "knockback_vector", Vector2.ZERO, duration)
 		animation.modulate = Color(1, 0, 0, 1)
 		knockback_tween.parallel().tween_property(animation, "modulate", Color(1, 1, 1, 1), duration)
+	
+	is_hurted = true
+	await get_tree().create_timer(0.3).timeout
+	is_hurted = false
+
+func _set_state():
+	var state = "idle"
+	
+	if !is_on_floor():
+		state = "jump"
+	elif direction != 0:
+		state = "run"
+	
+	if is_hurted:
+		state = "hurt"
+	
+	if animation.name != state:
+		animation.play(state)
 
 func _input(event):
 	if event is InputEventScreenTouch:
