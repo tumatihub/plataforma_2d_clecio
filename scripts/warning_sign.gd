2c41ffd773751acfa29d1e15782a6d294c6c65a7
tumatihub <jgsoaresdias@gmail.com>
2024-05-24 10:29:15 -0300

    Ep 16 - Criando Mensagens Pop ups na Godot 4.0

---
+++ scripts/warning_sign.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@onready var texture: Sprite2D = $texture
+@onready var area_sign: Area2D = $area_sign
+
+const lines: Array[String] = [
+	"Olá, aventureiro!",
+	"É muito bom vê-lo por aqui",
+	"Espero que esteja preparado...",
+	"Sua jornada está apenas...",
+	"...COMEÇANDO!",
+]
+
+func _unhandled_input(event: InputEvent) -> void:
+	if area_sign.get_overlapping_bodies().size() > 0:
+		texture.show()
+		if event.is_action_pressed("interact") && !DialogManager.is_message_active:
+			texture.hide()
+			DialogManager.start_message(global_position, lines)
+	else:
+		texture.hide()
+		if DialogManager.dialog_box != null:
+			DialogManager.dialog_box.queue_free()
+			DialogManager.is_message_active = false
+
+
