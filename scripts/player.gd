a041e9ee09bc79412cf311d061329d2ca82a6a62
tumatihub <jgsoaresdias@gmail.com>
2024-05-24 13:24:47 -0300

    Ep 19 - HUD do Game (Contador de TEMPO) Pt 2 na Godot 4.0

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,12 +8,13 @@ const JUMP_FORCE = -400.0
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_jumping := false
 var is_hurted := false
-var player_life := 10
 var knockback_vector := Vector2.ZERO
 var direction
 
 @onready var animation := $anim as AnimatedSprite2D
 @onready var remote_transform := $remote as RemoteTransform2D
+
+signal player_has_died()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -58,10 +59,11 @@ func follow_camera(camera):
 	remote_transform.remote_path = camera_path
 
 func take_damage(knockback_force := Vector2.ZERO, duration := 0.25):
-	if player_life > 0:
-		player_life -= 1
+	if Globals.player_life > 0:
+		Globals.player_life -= 1
 	else:
 		queue_free()
+		emit_signal("player_has_died")
 	
 	if knockback_force != Vector2.ZERO:
 		knockback_vector = knockback_force
--- scripts/world_01.gd
+++ scripts/world_01.gd
@@ -2,6 +2,16 @@ extends Node2D
 
 @onready var player := $player as CharacterBody2D
 @onready var camera := $camera as Camera2D
+@onready var control: Control = $HUD/control
 
 func _ready() -> void:
 	player.follow_camera(camera)
+	player.player_has_died.connect(reload_game)
+	control.time_is_up.connect(reload_game)
+	Globals.coins = 0
+	Globals.score = 0
+	Globals.player_life = 3
+
+func reload_game():
+	await get_tree().create_timer(1).timeout
+	get_tree().reload_current_scene()
