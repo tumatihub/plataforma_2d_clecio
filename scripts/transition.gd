c27cc9326f3f6b38a91f7f69645a366848e5a90b
tumatihub <jgsoaresdias@gmail.com>
2024-05-29 14:04:22 -0300

    Ep 24 - Como MUDAR de CENA na Godot 4.0 (PASSANDO DE FASE)

---
+++ scripts/transition.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+@onready var color_rect: ColorRect = $color_rect
+
+func _ready() -> void:
+	show_new_scene()
+
+func change_scene(path, delay = 2.5):
+	var scene_transition = get_tree().create_tween()
+	scene_transition.tween_property(color_rect, "threshold", 1.0, 0.5).set_delay(delay)
+	await scene_transition.finished
+	assert(get_tree().change_scene_to_file(path) == OK)
+
+func show_new_scene():
+	var show_transition = get_tree().create_tween()
+	show_transition.tween_property(color_rect, "threshold", 0.0, 0.5).from(1.0)
+	await show_transition.finished
