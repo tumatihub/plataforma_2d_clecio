45052f387ee3cb3deaef82dff6d3da60e07f1cb0
tumatihub <jgsoaresdias@gmail.com>
2024-05-29 21:04:43 -0300

    Ep 26 - BUGs CHECKPOINTs e RELOAD TIMER

--- scripts/world_01.gd
+++ scripts/world_01.gd
@@ -5,17 +5,20 @@ extends Node2D
 
 @onready var camera := $camera as Camera2D
 @onready var control: Control = $HUD/control
+@onready var player_start_position: Marker2D = $player_start_position
 
 func _ready() -> void:
+	Globals.player_start_position = player_start_position
 	Globals.player = player
 	Globals.player.follow_camera(camera)
 	Globals.player.player_has_died.connect(reload_game)
-	control.time_is_up.connect(reload_game)
+	control.time_is_up.connect(game_over)
 
 func reload_game():
 	await get_tree().create_timer(1).timeout
 	var player = player_scene.instantiate()
 	add_child(player)
+	control.reset_clock_timer()
 	Globals.player = player
 	Globals.player.follow_camera(camera)
 	Globals.player.player_has_died.connect(reload_game)
@@ -23,4 +26,6 @@ func reload_game():
 	Globals.score = 0
 	Globals.player_life = 3
 	Globals.respawn_player()
-	#get_tree().reload_current_scene()
+
+func game_over():
+	get_tree().reload_current_scene()
